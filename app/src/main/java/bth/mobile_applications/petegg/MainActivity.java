dbdde2b12e0eb57139f27bec79cac48d0a291f5a
rschuemann <rschuemann@stud.hs-bremen.de>
2019-12-02 17:25:37 +0100

    LoginScreen

--- app/src/main/java/bth/mobile_applications/petegg/MainActivity.java
+++ app/src/main/java/bth/mobile_applications/petegg/MainActivity.java
@@ -2,13 +2,89 @@ package bth.mobile_applications.petegg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * reminder: onCreate = main-function of the Activity window
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        animateBackground();
+        bouningEgg();
+        configureHomeButton();
     }
+
+    /**
+     * Switches to the next Activity
+     */
+    private void configureHomeButton(){
+        Button continueToHome = (Button) findViewById(R.id.continueToHome);
+
+        continueToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HomeScreen.class));
+            }
+        });
+    }
+
+    /**
+     * Moves the backgroundimages like the pet is walking through woods
+     */
+    private void animateBackground(){
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.backgroundTreeOne);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.backgroundTreeTwo);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(translationX - width);
+            }
+        });
+        animator.start();
+    }
+
+    /**
+     * Lets our egg bounce!
+     */
+    private void bouningEgg(){
+        final ImageView bouncingEgg = (ImageView) findViewById(R.id.bouncingEgg);
+
+        float f[] = new float[]{0f, 100f, 0f};
+
+        final ObjectAnimator animator = ObjectAnimator.ofFloat(bouncingEgg,"translationY",f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(1000L);
+        animator.start();
+    }
+
 }
